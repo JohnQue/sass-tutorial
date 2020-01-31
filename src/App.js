86e787cc0f433dbde913bbdf09e8f778afb10031
JohnQue <13outsider@naver.com>
2020-02-01 00:41:56 +0900

    Sass Practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/Button';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="buttons">
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button color="pink" size="small" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth className="customized-button">
+          BUTTON
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          BUTTON
+        </Button>
+        <Button
+          size="large"
+          color="pink"
+          fullWidth
+          onClick={() => console.log('클릭')}
+          onMouseMove={() => console.log('마우스 이동')}
         >
-          Learn React
-        </a>
-      </header>
+          BUTTON
+        </Button>
+      </div>
     </div>
   );
 }
